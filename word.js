17b837a5b72a15b49a48ada53861719af4e2d678
mgresty234 <mgresty223@gmail.com>
2018-05-31 18:51:43 -0400

    updated

--- word.js
+++ word.js
@@ -2,12 +2,19 @@ var Letter = require('./letter.js');
 
 var Word = function (wordGuess) {
     this.buildWord = function (word) {
-        for () {
+        for (var i = 0; i < this.guess.length; i++) {
+            console.log(i);
 
+        }
+        this.returnString = function() {
+
+        }
+        this.guessLetter = function() {
+            
         }
 
     }
     this.word = this.buildWord(wordGuess);
 }
 
-module.exports = word;
+//module.exports = word;
